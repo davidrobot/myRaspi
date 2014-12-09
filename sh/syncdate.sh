305a32c3a0c9fcf84097174177758a8da4db79b3
davidrobot <zero007@163.com>
2014-12-09 23:41:23 +0800

    add script use http to sync the date, could use http_proxy

---
+++ sh/syncdate.sh
@@ -0,0 +1,1 @@
+sudo date -s "$(wget -S  "http://www.baidu.com/" 2>&1 | grep -E '^[[:space:]]*[dD]ate:' | sed 's/^[[:space:]]*[dD]ate:[[:space:]]*//' | head -1l | awk '{print $1, $3, $2,  $5 ,"GMT", $4 }' | sed 's/,//')"
